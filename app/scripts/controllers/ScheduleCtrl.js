df0aca5991c154c160338fe0a66fa3ec291d522f
Michael Stewart <tgm@vt.edu>
2014-06-18 11:27:10 -0500

    little more data correction

--- app/scripts/controllers/ScheduleCtrl.js
+++ app/scripts/controllers/ScheduleCtrl.js
@@ -5,9 +5,9 @@ angular.module('enrollmentFrontendApp').controller('ScheduleCtrl', ['$scope', '$
     {title: 'Making Learning Visible Through Sustained Student Engagement', start: 'April 9, 2014', startdt:'2014-4-9', end: 'April 09, 2014', enddt:'2014-4-9',  theme: 'None',  credits: '1',  section: 366, role: 'learner'},
     {title: 'Field Trip to the Future - the Visionarium', start: 'March 7, 2014', startdt: '2014-3-7', end: 'March 7, 2014', enddt: '2014-3-7',  theme: 'None',  credits: '1',  section: 431, role: 'learner'},
     {title: 'University Libraries - Best Websites for Teaching and Instruction', start: 'March 3, 2014', end: 'March 03, 2014',  startdt: '2014-3-3', enddt: '2014-3-3', theme: 'None',  credits: '1',  section: 335, role: 'learner'},
-    {title: 'Seven Technology Tools to Use in Your Teaching Today', start: 'Feb. 06, 2014', end: 'Feb. 6, 2014', startdt: '2014-2-6', enddt: '2014-2-6',  theme: 'None',  credits: '1',  section: 313, role: 'coordinator'},
+    {title: 'Seven Technology Tools to Use in Your Teaching Today', start: 'Feb. 6, 2014', end: 'Feb. 6, 2014', startdt: '2014-2-6', enddt: '2014-2-6',  theme: 'None',  credits: '1',  section: 313, role: 'coordinator'},
     {title: 'Scholar - Engaging Students with Lessons', start: 'Feb. 4, 2014', end: 'Feb. 04, 2014', startdt: '2014-2-4', enddt: '2014-2-4',  theme: 'None',  credits: '1',  section: 387, role: 'instructor'},
-    {title: 'Designed Interaction Specialist Certificate', start: 'Feb. 4, 2014', end: 'April 15, 2014', startdt: '2014-4-15', enddt: '2014-4-15',  theme: 'Engaging Learners',  credits: '6*',  section: 430, role: 'learner'}
+    {title: 'Designed Interaction Specialist Certificate', start: 'April 15, 2014', end: 'April 15, 2014', startdt: '2014-4-15', enddt: '2014-4-15',  theme: 'Engaging Learners',  credits: '6*',  section: 430, role: 'learner'}
   ];
 
   $scope.searchQuery = '';
