91754e5e389490d4830c8747868b246030db46c1
Michael Stewart <tgm@vt.edu>
2014-07-07 11:13:04 -0400

    user service for authentication info

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -1,4 +1,18 @@
 'use strict';
-angular.module('enrollmentFrontendApp').service('User', ['$rootScope', function($rootScope){
-  this.loggedIn = true;
+angular.module('enrollmentFrontendUser', []).service('User', ['$rootScope', function($rootScope){
+  this.authenticated = false;
+  this.isElevated = false;
+  this.loggedIn = function() {
+    if (this.authenticated !== true) {
+      if (window.userId == ''){
+        return false;  
+      }
+      else {
+        this.id = window.userId;
+        return true;
+      }
+    } else {
+      return true;
+    }
+  }
 }]);
